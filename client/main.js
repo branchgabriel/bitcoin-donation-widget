a0efb79c7327db549ebf80e9bd74ab469ef62de0
gabriel <branchgabriel@gmail.com>
2014-02-28 23:25:39 -0500

    initial commit with coinbase-node code

---
+++ client/main.js
@@ -0,0 +1,11 @@
+var Coinbase = require("coinbase-node");
+//
+//var client = new Coinbase.Client({
+//    api_key: process.env.COINBASE_API_KEY
+//});
+//
+//function callback(err, resp, body){
+//    console.log(body);
+//}
+//
+//client.send_money('1G2UULb1M3hbB2b4Kt5v3E1R6Pc2XNW756','0.01', callack);
---
+++ test/client.js
@@ -0,0 +1,6 @@
+describe("client", function(){
+   var foo = bar;
+   it("foo is bar", function(){
+       expect(foo).toBe("bar") ;
+   });
+});
