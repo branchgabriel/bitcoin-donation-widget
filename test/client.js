0f784ac9e19f68be7add2ae71298454a71bd60e4
gabriel <gbranch@everesttech.com>
2014-03-01 18:56:08 -0500

    still unable to spy on BDW namespace

--- test/client.js
+++ test/client.js
@@ -1,6 +1,7 @@
 describe("client", function(){
   describe("init", function(){
     spyOn(BDW, 'highchartStart');
+    BDW.init();
     it("calls highchartStart", function(){
        expect(BDW.highchartStart).toHaveBeenCalled();
     });
