319a9b2655d6e19ba2f9d2cd9f06db1886d7ccf1
gabriel <gbranch@everesttech.com>
2014-03-29 15:18:41 -0400

    all tests now pass

--- test/clientSpec.js
+++ test/clientSpec.js
@@ -11,20 +11,24 @@ describe("client", function(){
        spyOn(BDW, 'getBalance');
        spyOn(BDW, 'highchartStart');
     });
-    BDW.init();
     it("sets the api key", function(){
+       BDW.init();
        expect(BDW.api_key).toBe("add yours here");
     });
     it("sets the base uril", function(){
+       BDW.init();
        expect(BDW.coinbase_uri).toBe('https://coinbase.com/api/v1');
     });
     it("calls events", function(){
+       BDW.init();
        expect(BDW.events).toHaveBeenCalled();
     });
     it("calls geBalance", function(){
+       BDW.init();
        expect(BDW.getBalance).toHaveBeenCalled();
     });
     it("calls highchartStart", function(){
+       BDW.init();
        expect(BDW.highchartStart).toHaveBeenCalled();
     });
   });
